1ab95cfd332b33782bcfbd1325aa7ea7a093b0db
jake-speed <jake.schubert.is@gmail.com>
2024-05-12 14:55:50 -0600

    Update bike-shop.sh

--- bike-shop.sh
+++ bike-shop.sh
@@ -9,11 +9,9 @@ MAIN_MENU() {
   then
     echo -e "\n$1"
   fi
-
   echo "How may I help you?" 
   echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
   read MAIN_MENU_SELECTION
-
   case $MAIN_MENU_SELECTION in
     1) RENT_MENU ;;
     2) RETURN_MENU ;;
@@ -23,29 +21,27 @@ MAIN_MENU() {
 }
 
 RENT_MENU() {
-  # get available bikes
+# get available bikes
   AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available = true ORDER BY bike_id")
-
-  # if no bikes available
+# if no bikes available
   if [[ -z $AVAILABLE_BIKES ]]
   then
-    # send to main menu
+# send to main menu
     MAIN_MENU "Sorry, we don't have any bikes available right now."
   else
-    # display available bikes
+# display available bikes
     echo -e "\nHere are the bikes we have available:"
     echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
     do
       echo "$BIKE_ID) $SIZE\" $TYPE Bike"
     done
-
-    # ask for bike to rent
+# ask for bike to rent
     echo -e "\nWhich one would you like to rent?"
     read BIKE_ID_TO_RENT
-    # if input is not a number
+# if input is not a number
     if [[ ! $BIKE_ID_TO_RENT =~ ^[0-9]+$ ]]
     then
-    # send to main menu
+# send to main menu
       MAIN_MENU "That is not a valid bike number."
     else
 #get bike availability
@@ -97,11 +93,7 @@ RETURN_MENU() {
     MAIN_MENU "I could not find a record for that phone number."
   else
 #get customer's rentals
-    CUSTOMER_RENTALS=$($PSQL "SELECT bike_id,type,size FROM bikes
-                              INNER JOIN rentals USING(bike_id)
-                              INNER JOIN customers USING(customer_id)
-                              WHERE phone = '$PHONE_NUMBER' AND date_returned IS NULL
-                              ORDER BY bike_id")
+    CUSTOMER_RENTALS=$($PSQL "SELECT bike_id,type,size FROM bikes INNER JOIN rentals USING(bike_id) INNER JOIN customers USING(customer_id) WHERE phone = '$PHONE_NUMBER' AND date_returned IS NULL ORDER BY bike_id")
 #if no rentals
     if [[ -z $CUSTOMER_RENTALS ]]
     then
